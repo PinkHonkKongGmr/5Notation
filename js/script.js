57351670d5bc38d0cced5c379813e32758883f8a
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-11-30 22:51:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
-
-
-
-var num=59;
+var inp1=document.querySelectorAll('.push')[0];
+var inp2=document.querySelectorAll('.push')[2];
+var res1=document.querySelectorAll('.push')[1];
+var res2=document.querySelectorAll('.push')[3];
+var show = document.querySelectorAll('.result')[0];
+var itog;
+var num;
 
 const five = 5;
 var remainder;
 var operation=[];
 
-while (num>0) {
-  remainder=num%five;
-  operation.push(remainder);
-  num-=remainder;
-  num=num/five;
+res1.onclick=function () {
+num= inp1.value;
+  while (num>0) {
+    remainder=num%five;
+    operation.push(remainder);
+    num-=remainder;
+    num=num/five;
 
+
+  }
+  itog=operation.reverse().join('');
+  show.innerHTML=itog;
 }
 
-var itog=operation.reverse().join('');
-console.log(itog);
 
 
-var fn=100;
+
+
+
+
+var fn=40;
 var arr=fn.toString().split('');
 var back=arr.length-1;
 var result=0;
