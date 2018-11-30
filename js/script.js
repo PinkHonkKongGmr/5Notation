bcd966e9ac7ae6a5298c9894d621c2a65c12c59e
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-11-30 17:15:13 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-console.log('a');
 
 
-var num;
+
+var num=5;
+
 const five = 5;
 var remainder;
+var operation=[];
 
-num=693;
-remainder=num%five;
-num-=remainder;
-num=num/five;
-console.log(num);
+while (num>0) {
+  remainder=num%five;
+  operation.push(remainder);
+  num-=remainder;
+  num=num/five;
+
+}
+
+var itog=operation.reverse().join('');
+console.log(itog);
